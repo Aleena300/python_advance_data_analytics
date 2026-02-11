a77c6a4526c1e5cefdf800848d23ee5d5b690f35
Aleena Alias <161939453+Aleena300@users.noreply.github.com>
2026-02-11 11:42:26 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,11 @@
+class python_advance_da:
+    def aleena():
+        print("aleena")
+    def walton():
+        print("walton")
+    def ankitha():
+        print("ankitha")
+
+
+python_advance_da.aleena()
+python_advance_da.walton()
---
+++ class2.py
@@ -0,0 +1,4 @@
+from class1 import python_advance_da
+from man import Management
+obj1= python_advance_da()
+obj2=Management()
